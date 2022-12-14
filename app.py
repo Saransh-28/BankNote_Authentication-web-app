02c357dca5cac1b9e1ed81fe9f82c06434962d04
Saransh Bhandari <83751767+Saransh-28@users.noreply.github.com>
2022-12-14 15:13:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 
-import numpy as np
+# import the required modules
 import pickle
-import pandas as pd
 import streamlit as st
 
+# load the model 
 pickle_in = open("model.pkl","rb")
 classifier=pickle.load(pickle_in)
 
+# function to return the predicted value
 def predict_note_authentication(variance,skewness,curtosis,entropy):
     prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
     if prediction == 0:
@@ -18,21 +19,27 @@ def predict_note_authentication(variance,skewness,curtosis,entropy):
 
 def main():
     st.title("Bank Note Authenticator")
+    
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:grey;text-align:center;"> Bank Note Authenticator ML Web App </h2>
     </div>
     """
+    
     st.markdown(html_temp , unsafe_allow_html=True)
+    
     variance=(st.text_input("variance"))
     skewness=(st.text_input("skewness"))
     curtosis=(st.text_input("curtosis"))
     entropy=(st.text_input("entropy"))
     
     result = ""
+    
     if st.button("Predict"):
         result = predict_note_authentication(float(variance),float(skewness),float(curtosis),float(entropy))
+        
     st.success(f'The prediction is -> {result}')
+    
 if __name__=='__main__':
     main()
     
